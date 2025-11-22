c6ab6a01a11c60ad24e11c73884656e4913fcb41
DenisNazarenko2604 <d.nazarenko2604@gmail.com>
2025-11-22 18:01:23 +0200

    Add files via upload

---
+++ Дз 4.py
@@ -0,0 +1,15 @@
+class Pets:
+    age = 10
+    name = "Animal"
+    weight = 4
+
+class Dogs(Pets):
+    age = 5
+    name = "Dog"
+    weight = 6
+
+class Cats(Pets):
+    age = 4
+    name = "Cat"
+    weight = 3
+
