19125cd39f86efbd4dcef44c043714835fbd0b9e
DenisNazarenko2604 <d.nazarenko2604@gmail.com>
2025-11-30 17:01:42 +0200

    Add files via upload

---
+++ Lesson 8.py
@@ -0,0 +1,18 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename="logs.log",
+                    filemode="w"
+                    format="We have new logging message: %(asctime)s:%(levelname)s - %(message)s"
+)
+
+logging.debug("debug")
+logging.info("info")
+logging.warning("warning")
+logging.error("error")
+logging.critical("critical")
+
+try:
+    print(10/0)
+except Exception as err:
+    logging.exception(f"16 рядок. Ошыбка - {err}")
